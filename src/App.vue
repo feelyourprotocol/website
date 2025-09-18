9da39d55b1a4ae6ee3112e99c8b2cd6d256a260d
Holger Drewes <Holger.Drewes@gmail.com>
2025-09-18 09:49:54 +0200

    Rename HelloWorld -> Footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <header>
     <div class="grid grid-cols-2 pt-2 pb-2 mb-5 border-b-2 border-blue-300">
-      <h1 class="font-mono text-lg">feelyourprotocol.org</h1>
+      <h1 class="font-mono text-lg">
+        <RouterLink to="/">feelyourprotocol.org</RouterLink>
+      </h1>
       <nav class="font-mono text-right">
-        <RouterLink to="/" class="ml-5">Home</RouterLink>
         <RouterLink to="/eip-7823-modexp-upper-bounds" class="ml-5">EIP-7823</RouterLink>
         <RouterLink to="/fusaka" class="ml-5">Fusaka</RouterLink>
       </nav>
@@ -17,7 +18,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
   <RouterView class="grid grid-cols-1" />
 
-  <HelloWorld msg="Made with ❤️ and pure dedication" />
+  <Footer msg="Made with ❤️ and pure dedication" />
 </template>
 
 <style scoped></style>
